d80d35f24e87c6b26c8bbc1f6ce930e205c83d1f
haroo <haroo@HAROON>
2025-02-11 18:23:46 +0100

    Add project files.

---
+++ EDMApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Message Sender App";
+IConnection conn = await factory.CreateConnectionAsync();
+IChannel channel = await conn.CreateChannelAsync();
+string exchangeName = "EDAExchange";
+string routingKey = "EDA-routing-key";
+string queueName = "EDAQueue";
+channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Direct);
+channel.QueueDeclareAsync(queueName,durable:false, exclusive:false,autoDelete:false,arguments:null);
+channel.QueueBindAsync(queueName,exchangeName,routingKey,arguments:null);
+byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello Wellcome to EDA");
+channel.BasicPublishAsync(exchangeName, routingKey,messageBodyBytes);
+channel.CloseAsync();
+conn.CloseAsync();
---
+++ MessageReceiver1/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.Uri = new Uri(uriString: "amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "EDA Receiver1";
+IConnection conn = await factory.CreateConnectionAsync();
+IChannel channel = await conn.CreateChannelAsync();
+string exchangename = "EDAExchange";
+string routingKey = "EDA-routing-key";
+string queueName = "EDAQueue";
+channel.ExchangeDeclareAsync(exchangename, ExchangeType.Direct);
+channel.QueueDeclareAsync(queueName,durable: false,exclusive:false,autoDelete:false,arguments:null);
+channel.QueueBindAsync(queueName,exchangename,routingKey,arguments:null);
+channel.BasicQosAsync(prefetchSize:0,prefetchCount:1,global:false);
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(value:$"Message Received:{ message}");
+    channel.BasicAckAsync(args.DeliveryTag, multiple: false);
+};
+string consumerTag = channel.BasicConsumeAsync(queueName, autoAck: false, consumer).ToString();
+Console.ReadLine();
+channel.BasicCancelAsync(consumerTag);
+channel.CloseAsync();
+conn.CloseAsync();
+
